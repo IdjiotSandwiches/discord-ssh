44fc0e570145eaf65536f3d01666b545a21dbca3
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-04-05 21:11:44 +0700

    Fix msg placement

--- util.py
+++ util.py
@@ -50,14 +50,12 @@ def tmux_kill(session_name: str):
     return subprocess.run(['tmux', 'kill-session', '-t', session_name], capture_output=True, text=True)
 
 def show_help():
-    cmd_help = """
-    --| List of command |--\n
-    - /ssh [username]: connect to SSH connection using username\n
-    - /tmux new [session_name]: create new session\n
-    - /tmux send [session_name] [command]: send & execute command to given session\n
-    - /tmux list: show all active session\n
-    - /tmux kill [session_name]: end given session\n
-    - /help: show list of commands\n
-    - /exit: close SSH connection
-    """
+    cmd_help = """--| List of command |--
+- /ssh [username]: connect to SSH connection using username
+- /tmux new [session_name]: create new session
+- /tmux send [session_name] [command]: send & execute command to given session
+- /tmux list: show all active session
+- /tmux kill [session_name]: end given session
+- /help: show list of commands
+- /exit: close SSH connection"""
     return cmd_help
